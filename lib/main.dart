ab21fb01ee5b491cc5eacbf7722544a980c0030e
Artur <arturbr@gmail.com>
2022-04-04 23:42:38 -0300

    Encapsulates question bank

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'quiz_brain.dart';
+
+QuizBrain questionBank = QuizBrain();
+
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -27,11 +31,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   int currentQuestion = 0;
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
 
   void nextQuestion(IconData? icon, Color color) {
     setState(() {
@@ -57,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[currentQuestion],
+                questionBank.getQuestion(currentQuestion),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -113,9 +112,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1:  false,
-question2:  true,
-question3:  true,
-*/
